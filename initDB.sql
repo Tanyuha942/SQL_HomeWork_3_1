dc405798741ac6e0cac857832834212bc427de4e
Tanya-PC <tanyuha942@gmail.com>
2021-10-19 00:23:12 +0300

    refactor hw_3_1

--- initDB.sql
+++ initDB.sql
@@ -1,14 +1,15 @@
 -- create database "db_test"
 --drop database db_test;
 --create database db_test;
--- gender dictionary
+
+ --gender dictionary
 create table gender (
    id     serial  not null,
    gender varchar not null unique,
    CONSTRAINT gender_pk PRIMARY KEY (id)
 );
 
--- projects dictionary
+ --projects dictionary
 create table projects(
 	id 		     serial      not null,
 	ProjectName  varchar(50) not null,
@@ -16,21 +17,34 @@ create table projects(
 	CONSTRAINT projects_pk PRIMARY KEY (id)
 );
 
--- customers dictionary
+ --customers dictionary
 create table customers(
 	id 		     serial      not null,
 	CustomerName varchar(50) not null,
 	CONSTRAINT customers_pk PRIMARY KEY (id)
 );
 
--- companies dictionary
+ --companies dictionary
 create table companies(
 	id 		     serial      not null,
 	CompanyName  varchar(50) not null,
 	CONSTRAINT companies_pk PRIMARY KEY (id)
 );
 
--- developers dictionary
+ --industry dictionary
+create table industry(
+	id 		     serial not null,
+	IndustryName varchar(50) not null,
+	CONSTRAINT industry_pk PRIMARY KEY (id)
+);
+ --skill_level dictionary
+create table skill_level(
+	id 		  serial not null,
+	LevelName varchar(50) not null,
+	CONSTRAINT skill_level_pk PRIMARY KEY (id)
+);
+
+ --developers dictionary
 create table developers(
 	id 		     serial not null,
 	LastName     varchar(50) not null,
@@ -39,45 +53,56 @@ create table developers(
 	DeveloperAge Integer,
 	DateOfBirth  date,
 	gender_id    Integer not null,
-	project_id   Integer not null,
 	company_id   Integer not null,
 	CONSTRAINT developers_pk PRIMARY KEY (id),
 	CONSTRAINT fk_id_gender FOREIGN KEY(gender_id) REFERENCES gender(id),
-	CONSTRAINT fk_id_project FOREIGN KEY(project_id) REFERENCES projects(id),
 	CONSTRAINT fk_id_company FOREIGN KEY(company_id) REFERENCES companies(id)
 );
 
--- industry dictionary
-create table industry(
-	id 		     serial not null,
-	IndustryName varchar(50) not null,
-	CONSTRAINT industry_pk PRIMARY KEY (id)
-);
--- skills_level dictionary
-create table skills_level(
-	id 		  serial not null,
-	LevelName varchar(50) not null,
-	CONSTRAINT skills_level_pk PRIMARY KEY (id)
-);
--- skills table
+
+ --skills table
 create table skills(
+	id 			   serial  not null,
 	Industry_Id    integer not null,
 	LevelSkills_Id integer not null,
-	Developer_Id   integer not null,
-	CONSTRAINT skills_pk PRIMARY KEY (Industry_Id, LevelSkills_Id, Developer_Id),
+	UNIQUE (Industry_Id, LevelSkills_Id),
+	CONSTRAINT skills_pk PRIMARY KEY (id),
 	CONSTRAINT fk_industry_id FOREIGN KEY(Industry_Id) REFERENCES industry(id),
-	CONSTRAINT fk_skills_level_id FOREIGN KEY(LevelSkills_Id) REFERENCES skills_level(id),
-	CONSTRAINT fk_developer_id FOREIGN KEY(Developer_Id) REFERENCES developers(id)
+	CONSTRAINT fk_skills_level_id FOREIGN KEY(LevelSkills_Id) REFERENCES skill_level(id)
 );
 
+-- developer_skill
+create table developer_skill(
+	developer_id integer not null,
+	skill_id     integer not null,
+	CONSTRAINT developer_skill_pk PRIMARY KEY (developer_id, skill_id),
+	CONSTRAINT fk_developers_developer_id FOREIGN KEY(developer_id) REFERENCES developers(id),
+	CONSTRAINT fk_skills_skill_id FOREIGN KEY(skill_id) REFERENCES skills(id)
+);
 
--- table of connection customers and companies
-create table customers_to_companies(
-	Customer_Id integer not null,
-	Company_Id  integer not null,
-	Project_id  integer not null, 
-	CONSTRAINT customers_to_companies_pk PRIMARY KEY (Customer_Id, Company_Id, Project_id),
-	CONSTRAINT fk_customer_id FOREIGN KEY(Customer_Id) REFERENCES customers(id),
-	CONSTRAINT fk_company_id FOREIGN KEY(Company_Id) REFERENCES companies(id),
-	CONSTRAINT fk_project_id FOREIGN KEY(Project_id) REFERENCES projects(id)
+-- developer_project
+create table developer_project(
+	developer_id integer not null,
+	project_id   integer not null,
+	CONSTRAINT developers_project_pk PRIMARY KEY (developer_id, project_id),
+	CONSTRAINT fk_developers_developer_id FOREIGN KEY(developer_id) REFERENCES developers(id),
+	CONSTRAINT fk_projects_project_id FOREIGN KEY(project_id) REFERENCES projects(id)
+);
+
+-- company_project
+create table company_project(
+	company_id integer not null,
+	project_id integer not null,
+	CONSTRAINT company_project_pk PRIMARY KEY (company_id, project_id),
+	CONSTRAINT fk_companies_company_id FOREIGN KEY(company_id) REFERENCES companies(id),
+	CONSTRAINT fk_projects_project_id FOREIGN KEY(project_id) REFERENCES projects(id)
+);
+
+-- customer_project
+create table customer_project(
+	customer_id integer not null,
+	project_id  integer not null,
+	CONSTRAINT customer_project_pk PRIMARY KEY (customer_id, project_id),
+	CONSTRAINT fk_customers_customer_id FOREIGN KEY(customer_id) REFERENCES customers(id),
+	CONSTRAINT fk_projects_project_id FOREIGN KEY(project_id) REFERENCES projects(id)
 );
--- populateDB.sql
+++ populateDB.sql
@@ -23,7 +23,7 @@ insert into companies (companyname)
 values ('Компания3');
 insert into companies (companyname)
 values ('Компания4');
---======================================================================================================--
+--======================================================================================================
 insert into customers (customername)
 values ('Заказчик1');
 insert into customers (customername)
@@ -32,39 +32,6 @@ insert into customers (customername)
 values ('Заказчик3');
 insert into customers (customername)
 values ('Заказчик4');
---======================================================================================================--
-insert into developers(lastname, firstname, surname, developerage, dateofbirth, gender_id, project_id, company_id)
-values('Сидоров', 'Валентин', 'Константинович', 27, to_date('21.02.1994', 'dd.mm.yyyy hh24:mi:ss'), 1, 3, 1);
-insert into developers(lastname, firstname, surname, developerage, dateofbirth, gender_id, project_id, company_id)
-values('Петров', 'Григорий', 'Антонович', 40, to_date('12.11.1981', 'dd.mm.yyyy hh24:mi:ss'), 1, 3, 1);
-insert into developers(lastname, firstname, surname, developerage, dateofbirth, gender_id, project_id, company_id)
-values('Самойлов', 'Валентин', 'Петрович', 30, to_date('05.07.1991', 'dd.mm.yyyy hh24:mi:ss'), 1, 3, 1);
-insert into developers(lastname, firstname, surname, developerage, dateofbirth, gender_id, project_id, company_id)
-values('Васильев', 'Анатолий', 'Константинович', 35, to_date('02.01.1986', 'dd.mm.yyyy hh24:mi:ss'), 1, 3, 1);
-insert into developers(lastname, firstname, surname, developerage, dateofbirth, gender_id, project_id, company_id)
-values('Маслова', 'Елена', 'Александровна', 29, to_date('12.11.1981', 'dd.mm.yyyy hh24:mi:ss'), 2, 3, 1);
-------------------------------------------------------------------------------------------------------------
-insert into developers(lastname, firstname, surname, developerage, dateofbirth, gender_id, project_id, company_id)
-values('Разработчик6', '', '', 27, to_date('12.11.1994', 'dd.mm.yyyy hh24:mi:ss'), 1, 2, 2);
-insert into developers(lastname, firstname, surname, developerage, dateofbirth, gender_id, project_id, company_id)
-values('Разработчик7', '', '', 40, to_date('30.12.1981', 'dd.mm.yyyy hh24:mi:ss'), 1, 2, 2);
-insert into developers(lastname, firstname, surname, developerage, dateofbirth, gender_id, project_id, company_id)
-values('Разработчик8', '', '', 30, to_date('11.03.1991', 'dd.mm.yyyy hh24:mi:ss'), 1, 2, 2);
-insert into developers(lastname, firstname, surname, developerage, dateofbirth, gender_id, project_id, company_id)
-values('Разработчик9', '', '', 35, to_date('22.09.1986', 'dd.mm.yyyy hh24:mi:ss'), 1, 2, 2);
-insert into developers(lastname, firstname, surname, developerage, dateofbirth, gender_id, project_id, company_id)
-values('Разработчик10', '', '', 29, to_date('14.06.1981', 'dd.mm.yyyy hh24:mi:ss'), 2, 2, 3);
-------------------------------------------------------------------------------------------------------------
-insert into developers(lastname, firstname, surname, developerage, gender_id, project_id, company_id)
-values('Разработчик11', '', '', 29, 1, 1, 3);
-insert into developers(lastname, firstname, surname, developerage, gender_id, project_id, company_id)
-values('Разработчик12', '', '', 31, 1, 1, 3);
-insert into developers(lastname, firstname, surname, developerage, gender_id, project_id, company_id)
-values('Разработчик13', '', '', 31, 1, 1, 3);
-insert into developers(lastname, firstname, surname, developerage, gender_id, project_id, company_id)
-values('Разработчик14', '', '', 26, 1, 1, 3);
-insert into developers(lastname, firstname, surname, developerage, gender_id, project_id, company_id)
-values('Разработчик15', '', '', 38, 1, 1, 3);
 --======================================================================================================--
 insert into industry (industryname)
 values ('Java');
@@ -75,35 +42,152 @@ values ('C#');
 insert into industry (industryname)
 values ('JS');
 --======================================================================================================--
-insert into skills_level (levelname)
+insert into skill_level (levelname)
 values('Junior');
-insert into skills_level (levelname)
+insert into skill_level (levelname)
 values('Middle');
-insert into skills_level (levelname)
+insert into skill_level (levelname)
 values('Senior');
 --======================================================================================================--
-insert into skills(industry_id, levelskills_id, developer_id)
-values(1, 3, 2);
-insert into skills(industry_id, levelskills_id, developer_id)
-values(2, 2, 1);
-insert into skills(industry_id, levelskills_id, developer_id)
-values(3, 1, 5);
-insert into skills(industry_id, levelskills_id, developer_id)
-values(4, 3, 7);
-insert into skills(industry_id, levelskills_id, developer_id)
-values(2, 1, 12);
+insert into developers(lastname, firstname, surname, developerage, dateofbirth, gender_id, company_id)
+values('Сидоров', 'Валентин', 'Константинович', 27, to_date('21.02.1994', 'dd.mm.yyyy hh24:mi:ss'), 1, 1);
+insert into developers(lastname, firstname, surname, developerage, dateofbirth, gender_id, company_id)
+values('Петров', 'Григорий', 'Антонович', 40, to_date('12.11.1981', 'dd.mm.yyyy hh24:mi:ss'), 1, 1);
+insert into developers(lastname, firstname, surname, developerage, dateofbirth, gender_id, company_id)
+values('Самойлов', 'Валентин', 'Петрович', 30, to_date('05.07.1991', 'dd.mm.yyyy hh24:mi:ss'), 1, 1);
+insert into developers(lastname, firstname, surname, developerage, dateofbirth, gender_id, company_id)
+values('Васильев', 'Анатолий', 'Константинович', 35, to_date('02.01.1986', 'dd.mm.yyyy hh24:mi:ss'), 1, 1);
+insert into developers(lastname, firstname, surname, developerage, dateofbirth, gender_id, company_id)
+values('Маслова', 'Елена', 'Александровна', 29, to_date('12.11.1981', 'dd.mm.yyyy hh24:mi:ss'), 2, 1);
+------------------------------------------------------------------------------------------------------------
+insert into developers(lastname, firstname, surname, developerage, dateofbirth, gender_id, company_id)
+values('Разработчик6', '', '', 27, to_date('12.11.1994', 'dd.mm.yyyy hh24:mi:ss'), 1, 2);
+insert into developers(lastname, firstname, surname, developerage, dateofbirth, gender_id, company_id)
+values('Разработчик7', '', '', 40, to_date('30.12.1981', 'dd.mm.yyyy hh24:mi:ss'), 1, 2);
+insert into developers(lastname, firstname, surname, developerage, dateofbirth, gender_id, company_id)
+values('Разработчик8', '', '', 30, to_date('11.03.1991', 'dd.mm.yyyy hh24:mi:ss'), 1, 2);
+insert into developers(lastname, firstname, surname, developerage, dateofbirth, gender_id, company_id)
+values('Разработчик9', '', '', 35, to_date('22.09.1986', 'dd.mm.yyyy hh24:mi:ss'), 1, 2);
+insert into developers(lastname, firstname, surname, developerage, dateofbirth, gender_id, company_id)
+values('Разработчик10', '', '', 29, to_date('14.06.1981', 'dd.mm.yyyy hh24:mi:ss'), 2, 3);
+------------------------------------------------------------------------------------------------------------
+insert into developers(lastname, firstname, surname, developerage, gender_id, company_id)
+values('Разработчик11', '', '', 29, 1, 3);
+insert into developers(lastname, firstname, surname, developerage, gender_id, company_id)
+values('Разработчик12', '', '', 31, 1, 3);
+insert into developers(lastname, firstname, surname, developerage, gender_id, company_id)
+values('Разработчик13', '', '', 31, 1, 3);
+insert into developers(lastname, firstname, surname, developerage, gender_id, company_id)
+values('Разработчик14', '', '', 26, 1, 3);
+insert into developers(lastname, firstname, surname, developerage, gender_id, company_id)
+values('Разработчик15', '', '', 38, 1, 3);
 --======================================================================================================--
-insert into customers_to_companies(customer_id, company_id, project_id) -- 4 - 4 - 5
-values(1, 4, 5);
-insert into customers_to_companies(customer_id, company_id, project_id)
-values(2, 3, 4);
-insert into customers_to_companies(customer_id, company_id, project_id)
-values(3, 2, 3);
-insert into customers_to_companies(customer_id, company_id, project_id)
-values(4, 1, 2);
-insert into customers_to_companies(customer_id, company_id, project_id)
-values(4, 2, 1);
+insert into skills(industry_id, levelskills_id)
+values(1, 1);
+insert into skills(industry_id, levelskills_id)
+values(1, 2);
+insert into skills(industry_id, levelskills_id)
+values(1, 3);
+insert into skills(industry_id, levelskills_id)
+values(2, 1);
+insert into skills(industry_id, levelskills_id)
+values(2, 2);
+insert into skills(industry_id, levelskills_id)
+values(2, 3);
+insert into skills(industry_id, levelskills_id)
+values(3, 1);
+insert into skills(industry_id, levelskills_id)
+values(3, 2);
+insert into skills(industry_id, levelskills_id)
+values(3, 3);
+insert into skills(industry_id, levelskills_id)
+values(4, 1);
+insert into skills(industry_id, levelskills_id)
+values(4, 2);
+insert into skills(industry_id, levelskills_id)
+values(4, 3);
 --======================================================================================================--
+insert into developer_skill(developer_id, skill_id)
+values(2, 3);
+insert into developer_skill(developer_id, skill_id)
+values(1, 5);
+insert into developer_skill(developer_id, skill_id)
+values(5, 7);
+insert into developer_skill(developer_id, skill_id)
+values(4, 12);
+-----------------------------------------------------------------------------------------------
+insert into developer_project(developer_id, project_id)
+values(1, 3);
+insert into developer_project(developer_id, project_id)
+values(2, 3);
+insert into developer_project(developer_id, project_id)
+values(3, 3);
+insert into developer_project(developer_id, project_id)
+values(4, 3);
+insert into developer_project(developer_id, project_id)
+values(5, 3);
+-----------------------------------------------------------------------------------------------
+insert into developer_project(developer_id, project_id)
+values(6, 2);
+insert into developer_project(developer_id, project_id)
+values(7, 2);
+insert into developer_project(developer_id, project_id)
+values(8, 2);
+insert into developer_project(developer_id, project_id)
+values(9, 2);
+insert into developer_project(developer_id, project_id)
+values(10, 2);
+-----------------------------------------------------------------------------------------------
+insert into developer_project(developer_id, project_id)
+values(11, 1);
+insert into developer_project(developer_id, project_id)
+values(12, 1);
+insert into developer_project(developer_id, project_id)
+values(13, 1);
+insert into developer_project(developer_id, project_id)
+values(14, 1);
+insert into developer_project(developer_id, project_id)
+values(15, 1);
+--======================================================================================================--
+insert into customer_project(customer_id, project_id)
+values(1, 5);
+insert into customer_project(customer_id, project_id)
+values(2, 4);
+insert into customer_project(customer_id, project_id)
+values(3, 3);
+insert into customer_project(customer_id, project_id)
+values(4, 2);
+insert into customer_project(customer_id, project_id)
+values(4, 1);
+--======================================================================================================--
+insert into company_project(company_id, project_id)
+values(4, 5);
+insert into company_project(company_id, project_id)
+values(3, 4);
+insert into company_project(company_id, project_id)
+values(2, 3);
+insert into company_project(company_id, project_id)
+values(1, 2);
+insert into company_project(company_id, project_id)
+values(2, 1);
+--======================================================================================================--
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
